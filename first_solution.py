68738296731417031310c7897bfafd50b3fcd4af
Alejo <alejoivanrojas@gmail.com>
2025-04-06 18:00:12 -0300

    delete unused index

--- first_solution.py
+++ first_solution.py
@@ -4,16 +4,16 @@ def first_solution():
     max_value_key = None
     max_value = 0
 
-    for index, i in enumerate(list):
-        if i in reference_values:
-            reference_values[i] += 1
+    for element in list:
+        if element in reference_values:
+            reference_values[element] += 1
         else:
-            reference_values[i] = 1
+            reference_values[element] = 1
 
         # Update the most frequent element on the same loop, avoiding the need to iterate through the dictionary again
-        if reference_values[i] > max_value:
-            max_value = reference_values[i]
-            max_value_key = i
+        if reference_values[element] > max_value:
+            max_value = reference_values[element]
+            max_value_key = element
         
         #Early return if the most frequent value is greater than half of the list
         if max_value > len(list) / 2:
